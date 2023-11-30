8677fb24a8fa79ec36d73dcd7c2865033afd03b7
mohnas <mn9142001@gmail.com>
2023-11-30 19:27:05 +0200

    alter drf jwt token lifetime

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,6 +77,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'project.wsgi.application'
 
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=365),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1024),
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
